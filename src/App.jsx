8751d0da87c458a3cd05bd99353501b642bfb7df
Aman Thukral <thukral618@gmail.com>
2023-08-06 14:06:50 +0530

    Adding logic for protected routes

--- src/App.jsx
+++ src/App.jsx
@@ -1,27 +1,22 @@
-import { createBrowserRouter, RouterProvider } from "react-router-dom";
+import { Routes, Route } from "react-router-dom";
 import "./App.css";
 import Signin from "./components/auth/Signin";
 import Signup from "./components/auth/Signup";
 import Home from "./components/dashboard/Home";
+import ProtectedRoute from "./components/ProtectedRoute";
 
 function App() {
-  const router = createBrowserRouter([
-    {
-      path: "/",
-      element: <Home />,
-    },
-    {
-      path: "/signin",
-      element: <Signin />,
-    },
-    {
-      path: "/signup",
-      element: <Signup />,
-    },
-  ]);
   return (
     <>
-      <RouterProvider router={router}></RouterProvider>
+      <div>
+        <Routes>
+          <Route element={<ProtectedRoute />}>
+            <Route path="/" element={<Home />} />
+          </Route>
+          <Route path="/signin" element={<Signin />} />
+          <Route path="/signup" element={<Signup />} />
+        </Routes>
+      </div>
     </>
   );
 }
---
+++ src/components/ProtectedRoute.jsx
@@ -0,0 +1,14 @@
+import { Navigate, Outlet, useLocation } from "react-router-dom";
+import { UserAuth } from "../contexts/AuthProvider";
+
+const ProtectedRoute = () => {
+  const { user } = UserAuth();
+  const location = useLocation();
+  return user ? (
+    <Outlet />
+  ) : (
+    <Navigate to={"/signin"} replace state={{ path: location.pathname }} />
+  );
+};
+
+export default ProtectedRoute;
