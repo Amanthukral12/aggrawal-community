1e12b54572171d6de2e9517cceb424fbb7bb29b0
Aman Thukral <thukral618@gmail.com>
2023-08-18 13:36:09 +0530

    Adding dashboard structure and styling

--- src/components/dashboard/Home.jsx
+++ src/components/dashboard/Home.jsx
@@ -1,13 +1,24 @@
 import { Link } from "react-router-dom";
-
-import { UseProfile } from "../../contexts/ProfileContext";
+import "./styles.css";
+import { HiUserGroup } from "react-icons/hi";
+import { BsCalendarEventFill } from "react-icons/bs";
+import { GiBigDiamondRing } from "react-icons/gi";
 
 const Home = () => {
-  const { currentProfile } = UseProfile();
-
   return (
-    <div>
-      <span>Welcome {currentProfile.first_name}</span>
+    <div className="mainDiv">
+      <Link to={"/members"} className="section">
+        <HiUserGroup className="icons" />
+        <h2>Memebers</h2>
+      </Link>
+      <Link to={"/events"} className="section">
+        <BsCalendarEventFill className="icons" />
+        <h2>Events</h2>
+      </Link>
+      <Link to={"/posts"} className="section">
+        <GiBigDiamondRing className="icons" />
+        <h2>Rishtas</h2>
+      </Link>
     </div>
   );
 };
