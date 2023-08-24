3951cf6da9737e846d68f701c31d09d512867e8c
Aman Thukral <thukral618@gmail.com>
2023-08-24 18:53:45 +0530

    Added PWA support to make app installable

--- vite.config.js
+++ vite.config.js
@@ -1,8 +1,39 @@
 import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react-swc";
 import viteCompression from "vite-plugin-compression";
+import { VitePWA } from "vite-plugin-pwa";
+
+const manifestForPlugin = {
+  registerType: "prompt",
+  manifest: {
+    name: "Aggrawal Community",
+    short_name: "Aggrawal Community",
+    description: "A Community App",
+    icons: [
+      {
+        src: "./assets/Logo.png",
+        sizes: "512x512",
+        type: "image/png",
+        purpose: "maskable",
+      },
+      {
+        src: "./assets/Logo.png",
+        sizes: "512x512",
+        type: "image/png",
+        purpose: "any",
+      },
+    ],
+    theme_color: "#000",
+    background_color: "#000",
+    display: "standalone",
+    scope: "/",
+    start_url: "/",
+    orientation: "portrait",
+  },
+};
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [react(), viteCompression()],
+  base: "./",
+  plugins: [react(), viteCompression(), VitePWA(manifestForPlugin)],
 });
